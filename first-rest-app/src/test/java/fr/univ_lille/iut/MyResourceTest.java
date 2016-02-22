0e9cc23070d0fab46e41939e65a3068d7bbec1cf
RST <rudy.16@hotmail.fr>
2016-02-22 11:54:37 +0100

    add test class

---
+++ first-rest-app/src/test/java/fr/univ_lille/iut/MyResourceTest.java
@@ -0,0 +1,26 @@
+package fr.univ_lille.iut;
+
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MyResourceTest extends JerseyTest 
+{
+    @Override 
+    protected Application configure() 
+    {
+        return new ResourceConfig(MyResource.class);
+    }
+    
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() 
+    {
+        String responseMsg = target("/myresource").request().get(String.class);
+        assertEquals("Got it!", responseMsg);
+    }
+}
